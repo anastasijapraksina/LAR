cf3ff55033e64840b413630e4636987e0e967991
anastasijapraksina <144609119+anastasijapraksina@users.noreply.github.com>
2023-09-11 12:36:58 +0300

    result.py

---
+++ result.py
@@ -0,0 +1,19 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? ").rstrip())
+    percent = percent_to_float(input("What percentage would you like to tip? ").rstrip())
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+def dollars_to_float(d):  
+    t = d.strip()
+    t1 = t.replace("$"," ")
+    r=float(t1)
+    return (r)
+
+def percent_to_float(p):  
+    t2 = p.strip()
+    t3 = t2.replace("%"," ")
+    r=float(t2)
+    return (r/100)
+
+main()
